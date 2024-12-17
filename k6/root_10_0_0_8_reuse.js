c1a6c80b0fd079fec0bbf3038ada77bb2e719691
Sander Saares <sander@saares.eu>
2024-12-17 11:24:48 +0200

    fix bad code

--- k6/root_10_0_0_8_reuse.js
+++ k6/root_10_0_0_8_reuse.js
@@ -1,9 +1,11 @@
 import http from 'k6/http';
 
 export const options = {
-  timeout: "1s",
 };
 
 export default function () {
-  http.get('http://10.0.0.8:3000/');
+  let params = {
+    timeout: "1s",
+  };
+  http.get('http://10.0.0.8:3000/', params);
 }

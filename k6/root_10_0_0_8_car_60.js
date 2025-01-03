33c8874e615b9b0e3644dad9e19cdf9af15a2a7a
Sander Saares <sander@saares.eu>
2025-01-03 10:31:29 +0200

    car

---
+++ k6/root_10_0_0_8_car_60.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+
+export const options = {
+  // Avoid the extreme hammering if the server starts rejecting us.
+  minIterationDuration: '10ms',
+
+  scenarios: {
+    contacts: {
+      executor: 'constant-arrival-rate',
+      duration: '60s',
+      rate: 60,
+      timeUnit: '1s',
+      preAllocatedVUs: 4,
+      maxVUs: 100,
+    },
+  },
+};
+
+export default function () {
+  let params = {
+    // Reasonable SLA - anything longer than this is failed request.
+    timeout: "500ms",
+  };
+  http.get('http://10.0.0.8:3000/', params);
+}
